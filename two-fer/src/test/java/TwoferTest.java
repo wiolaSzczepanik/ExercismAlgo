7062756434aecb55eb71cf8489e1c475578f91cf
wiolaSzczepanik <wiolaszcz@gmail.com>
2018-10-18 16:09:37 +0200

    comment @Ignore to run all tests

--- two-fer/src/test/java/TwoferTest.java
+++ two-fer/src/test/java/TwoferTest.java
@@ -21,7 +21,7 @@ public class TwoferTest {
         assertEquals(expected, twofer.twofer(input));
     }
 
-    @Ignore("Remove to run test")
+//    @Ignore("Remove to run test")
     @Test
     public void aNameGiven() {
         String input = "Alice";
@@ -30,7 +30,7 @@ public class TwoferTest {
         assertEquals(expected, twofer.twofer(input));
     }
 
-    @Ignore("Remove to run test")
+//    @Ignore("Remove to run test")
     @Test
     public void anotherNameGiven() {
         String input = "Bob";
